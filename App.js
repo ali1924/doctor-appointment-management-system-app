314faa4affa9bdca3a32e654d6005362f9275ef3
ali1924 <mohammadali229987@gmail.com>
2023-12-03 23:32:47 +0600

    Google Sign in done

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './App/Screens/Login';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import Login from "./App/Screens/Login";
+import SignInWithOAuth from "./App/Components/SignInWithOAuth";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Login/>
-      {/* <StatusBar style="auto" /> */}
-    </View>
+    <ClerkProvider
+      publishableKey={
+        "pk_test_aW5jbHVkZWQtZmFsY29uLTMuY2xlcmsuYWNjb3VudHMuZGV2JA"
+      }
+    >
+      <SafeAreaView style={styles.container}>
+        {/* Sign IN */}
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        {/* Sign Out */}
+        <SignedOut>
+          {/* <Text>You are Signed out</Text> */}
+          <SignInWithOAuth />
+        </SignedOut>
+      </SafeAreaView>
+    </ClerkProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
